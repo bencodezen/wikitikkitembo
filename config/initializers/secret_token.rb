a54d96e3b0a51328064f903dfecb419ab60f32e6
bengozen <bengozen@gmail.com>
2014-11-10 00:00:13 -0500

    Created baseline rails app with Readme.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikitikkitembo::Application.config.secret_key_base = '54405f8887856f7c5a164188b036c6b1ef7d9633b0a1baeffd976ae454ae43f1365ad297a754207ab52c0f9c6723ca86bb276523d4d86cec00221f4054dd595d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wikitikkitembo::Application.config.session_store :cookie_store, key: '_wikitikkitembo_session'
