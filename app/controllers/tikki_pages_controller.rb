7e82fe8d098289cf0dda456ce1244e55057f9f8e
bengozen <bengozen@gmail.com>
2014-11-12 16:19:54 -0500

    TikkiPages can be created, edited, and viewed.

--- app/controllers/tikki_pages_controller.rb
+++ app/controllers/tikki_pages_controller.rb
@@ -1,13 +1,41 @@
 class TikkiPagesController < ApplicationController
   def index
+    @tikki_pages = TikkiPage.all
   end
 
   def show
+    @tikki_page = TikkiPage.find(params[:id])
   end
 
   def new
+    @tikki_page = TikkiPage.new
+  end
+
+  def create
+    @tikki_page = TikkiPage.new(tikki_page_params)
+    if @tikki_page.save
+      redirect_to @tikki_page
+    else
+      redirect_to root_path
+    end
   end
 
   def edit
+    @tikki_page = TikkiPage.find(params[:id])
+  end
+
+  def update
+    @tikki_page = TikkiPage.find(params[:id])
+    if @tikki_page.update_attributes(tikki_page_params)
+      redirect_to @tikki_page
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def tikki_page_params
+    params.require(:tikki_page).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Wikitikkitembo::Application.routes.draw do
   devise_for :users
-  resources :tikkipages
+  resources :tikki_pages
   get 'about' => "welcome/index"
 
   root 'welcome#index'
